1dac521bfc766d2737d41532fc22bc271a323514
Deeds <romaz20@ukr.net>
2020-05-15 13:38:59 +0300

    release code

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $(".main-slider-data").slick({
+    autoplay: false,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  });
+});
